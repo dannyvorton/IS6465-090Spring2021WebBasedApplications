b2cb6d370d9962eb86a4a801f3878d7f248694f9
yagobaby <78980927+yagobaby@users.noreply.github.com>
2021-02-22 23:44:22 -0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,41 @@
+<html>
+	<head></head>
+		<title>Urban Outfitters</title>
+		<link rel="stylesheet" href="/urbanoutfitters/stylesheets/styles.css">
+	<body>
+	<!-- Test code
+		<div id="login">
+			<img src="/urbanoutfitters/images/UO_LOGO-XL.png" height="3%"><br>
+					
+			<form action="/homework/card-list.html" method="get">
+				Username: <input type="text" name="username"><br>
+				Password: <input type="password" name="password"><br>
+				<input type="submit" value="Sign In">
+			</form>
+		</div>
+	-->	
+		<div id="login">
+			
+			<form id="form_login", action="/urbanoutfitters/profile.php" method="get" >
+				<img src="/urbanoutfitters/images/UO_LOGO-XL.png" height="3%"><br><br>
+				Username: <br>
+				<input type="text" name="username"><br>
+				Password: <br>
+				<input type="password" name="password"><br>
+				<input type="submit" value="Sign In">
+				<br><br><br>
+			</form>
+		</div>
+		<div id="newuser">
+			<form id="form_newuser", action="/urbanoutfitters/newuser.php" method="get" >
+				New User: <br>
+				<input type="submit" value="Sign Up">
+				<br>
+			</form>	
+		</div>
+		
+	</body>
+
+
+
+</html>
