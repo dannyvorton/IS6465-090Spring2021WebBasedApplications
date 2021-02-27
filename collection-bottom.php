38c50930cce1d1f6afa0be564735c10a17febfcf
Dan Orton <52336508+dannyvorton@users.noreply.github.com>
2021-02-26 21:44:19 -0700

    add images

---
+++ collection-bottom.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="description" content="Suburban Outfitters">
+        <meta name="keywords" content="HTML, CSS, JavaScript, PHP, MySQL">
+        <meta name="viewpoint" content="width=device-width, initial-scale=1.0">
+        <meta name="author" content="Amelia Le, Dave Hyde, Dan Orton">
+        <meta name="date" content="February 22, 2021">
+        <link rel="stylesheet" href="/styles.css">
+        <title>Suburban Outfitters Collection</title>
+    </head>
+    <body>
+        <!-- BOTTOM -->
+        <img src="/IS6465-GroupProject/image/FlapPocketCordPants.jpg" alt="Flap Pocket Cord Pants" width="100" height="100">
+        <img src="/IS6465-GroupProject/image/SHEINFlareLegRib-knitPants.jpg" alt="SHEIN Flare Leg Rib-knit Pants" width="100" height="100">
+        <img src="/IS6465-GroupProject/image/SHEINHighWaistPlaidSkinnyPants.jpg" alt="SHEIN High Waist Plaid Skinny Pants" width="100" height="100">
+        <img src="/IS6465-GroupProject/image/SHEINRuchedSolidPants.jpg" alt="SHEIN Ruched Solid Pants" width="100" height="100">
+        <img src="/IS6465-GroupProject/image/SHEINTieDyeElasticWaistFlareLegPants.jpg" alt="SHEIN Tie Dye Elastic Waist Flare Leg Pants" width="100" height="100">
+        <img src="/IS6465-GroupProject/image/TieFrontSolidTaperedPants.jpg" alt="Tie Front Solid Tapered Pants" width="100" height="100">
+        <img src="/IS6465-GroupProject/image/ZipUpGraphicPrintFlareLegPants.jpg" alt="Zip Up Graphic Print Flare Leg Pants" width="100" height="100">
+    </body>
+</html>
---
+++ collection-dress.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="description" content="Suburban Outfitters">
+        <meta name="keywords" content="HTML, CSS, JavaScript, PHP, MySQL">
+        <meta name="viewpoint" content="width=device-width, initial-scale=1.0">
+        <meta name="author" content="Amelia Le, Dave Hyde, Dan Orton">
+        <meta name="date" content="February 22, 2021">
+        <link rel="stylesheet" href="/styles.css">
+        <title>Suburban Outfitters Collection</title>
+    </head>
+    <body>
+        <!-- DRESS -->
+        <img src="/IS6465-GroupProject/image/AlloverPrintSwingA-lineDress.jpg" alt="Allover Print Swing A-line Dress" width="100" height="100">
+        <img src="/IS6465-GroupProject/image/ButtonDetailSleevelessFittedDress.jpg" alt="Button Detail Sleeveless Fitted Dress" width="100" height="100">
+        <img src="/IS6465-GroupProject/image/DitsyFloralPrintSweetheartNeckSlitHemDress.jpg" alt="Ditsy Floral Print Sweetheart Neck Slit Hem Dress" width="100" height="100">
+        <img src="/IS6465-GroupProject/image/HalfZipThumbHoleBodyconDress.jpg" alt="Half Zip Thumb Hole Bodycon Dress" width="100" height="100">
+        <img src="/IS6465-GroupProject/image/RuchedSolidBodyconDress.jpg" alt="Ruched Solid Bodycon Dress" width="100" height="100">
+        <img src="/IS6465-GroupProject/image/SHEINClipDotChiffonRuchedSelf-TieBodiceMaxiDress.jpg" alt="SHEIN Clip Dot Chiffon Ruched Self-Tie Bodice Maxi Dress" width="100" height="100">
+        <img src="/IS6465-GroupProject/image/StripedSplitThighTeeDress.jpg" alt="Striped Split Thigh Tee Dress" width="100" height="100">
+    </body>
+</html>
