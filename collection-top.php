68fe1ebc52185afd0dbd294ce935c573ccbe4531
Dan Orton <52336508+dannyvorton@users.noreply.github.com>
2021-02-26 21:47:10 -0700

    add images

--- collection-top.php
+++ collection-top.php
@@ -11,6 +11,13 @@
         <title>Suburban Outfitters Collection</title>
     </head>
     <body>
-        
+        <!-- TOP -->
+        <img src="/IS6465-GroupProject/image/ContrastCollarHoundstoothBlouse.jpg" alt="Contrast Collar Houndstooth Blouse" width="100" height="100">
+        <img src="/IS6465-GroupProject/image/FloralEmbroideryFlounceSleeveBlouse.jpg" alt="Floral Embroidery Flounce Sleeve Blouse" width="100" height="100">
+        <img src="/IS6465-GroupProject/image/PlaidPrintButtonFrontOvershirt.jpg" alt="Plaid Print Button Front Overshirt" width="100" height="100">
+        <img src="/IS6465-GroupProject/image/PolkaDotFlounceSleeveBabydollBlouse.jpg" alt="Polka Dot Flounce Sleeve Babydoll Blouse" width="100" height="100">
+        <img src="/IS6465-GroupProject/image/SHEINBASICS2PackSolidCropTee.jpg" alt="SHEIN BASICS 2 Pack Solid Crop Tee" width="100" height="100">
+        <img src="/IS6465-GroupProject/image/SHEINV-cutBackTwistDetailTropicalTop.jpg" alt="SHEIN V-cut Back Twist Detail Tropical Top" width="100" height="100">
+        <img src="/IS6465-GroupProject/image/SurpliceFrontBatwingSleeveTieFrontBlouse.jpg" alt="Surplice Front Batwing Sleeve Tie Front Blouse" width="100" height="100">
     </body>
 </html>
