8d900752e2d19a82f3999d183a27920c27f58c13
Dan Orton <52336508+dannyvorton@users.noreply.github.com>
2021-02-26 21:41:43 -0700

    add images

--- collection.php
+++ collection.php
@@ -11,6 +11,29 @@
         <title>Suburban Outfitters Collection</title>
     </head>
     <body>
-        <img src="/IS6465-GroupProject/image/AlloverPrintSwingA-lineDress.jpg"><br><br>
+        <!-- DRESS -->
+        <img src="/IS6465-GroupProject/image/AlloverPrintSwingA-lineDress.jpg" alt="Allover Print Swing A-line Dress" width="100" height="100">
+        <img src="/IS6465-GroupProject/image/ButtonDetailSleevelessFittedDress.jpg" alt="Button Detail Sleeveless Fitted Dress" width="100" height="100">
+        <img src="/IS6465-GroupProject/image/DitsyFloralPrintSweetheartNeckSlitHemDress.jpg" alt="Ditsy Floral Print Sweetheart Neck Slit Hem Dress" width="100" height="100">
+        <img src="/IS6465-GroupProject/image/HalfZipThumbHoleBodyconDress.jpg" alt="Half Zip Thumb Hole Bodycon Dress" width="100" height="100">
+        <img src="/IS6465-GroupProject/image/RuchedSolidBodyconDress.jpg" alt="Ruched Solid Bodycon Dress" width="100" height="100">
+        <img src="/IS6465-GroupProject/image/SHEINClipDotChiffonRuchedSelf-TieBodiceMaxiDress.jpg" alt="SHEIN Clip Dot Chiffon Ruched Self-Tie Bodice Maxi Dress" width="100" height="100">
+        <img src="/IS6465-GroupProject/image/StripedSplitThighTeeDress.jpg" alt="Striped Split Thigh Tee Dress" width="100" height="100">
+        <!-- TOP -->
+        <img src="/IS6465-GroupProject/image/ContrastCollarHoundstoothBlouse.jpg" alt="Contrast Collar Houndstooth Blouse" width="100" height="100">
+        <img src="/IS6465-GroupProject/image/FloralEmbroideryFlounceSleeveBlouse.jpg" alt="Floral Embroidery Flounce Sleeve Blouse" width="100" height="100">
+        <img src="/IS6465-GroupProject/image/PlaidPrintButtonFrontOvershirt.jpg" alt="Plaid Print Button Front Overshirt" width="100" height="100">
+        <img src="/IS6465-GroupProject/image/PolkaDotFlounceSleeveBabydollBlouse.jpg" alt="Polka Dot Flounce Sleeve Babydoll Blouse" width="100" height="100">
+        <img src="/IS6465-GroupProject/image/SHEINBASICS2PackSolidCropTee.jpg" alt="SHEIN BASICS 2 Pack Solid Crop Tee" width="100" height="100">
+        <img src="/IS6465-GroupProject/image/SHEINV-cutBackTwistDetailTropicalTop.jpg" alt="SHEIN V-cut Back Twist Detail Tropical Top" width="100" height="100">
+        <img src="/IS6465-GroupProject/image/SurpliceFrontBatwingSleeveTieFrontBlouse.jpg" alt="Surplice Front Batwing Sleeve Tie Front Blouse" width="100" height="100">
+        <!-- BOTTOM -->
+        <img src="/IS6465-GroupProject/image/FlapPocketCordPants.jpg" alt="Flap Pocket Cord Pants" width="100" height="100">
+        <img src="/IS6465-GroupProject/image/SHEINFlareLegRib-knitPants.jpg" alt="SHEIN Flare Leg Rib-knit Pants" width="100" height="100">
+        <img src="/IS6465-GroupProject/image/SHEINHighWaistPlaidSkinnyPants.jpg" alt="SHEIN High Waist Plaid Skinny Pants" width="100" height="100">
+        <img src="/IS6465-GroupProject/image/SHEINRuchedSolidPants.jpg" alt="SHEIN Ruched Solid Pants" width="100" height="100">
+        <img src="/IS6465-GroupProject/image/SHEINTieDyeElasticWaistFlareLegPants.jpg" alt="SHEIN Tie Dye Elastic Waist Flare Leg Pants" width="100" height="100">
+        <img src="/IS6465-GroupProject/image/TieFrontSolidTaperedPants.jpg" alt="Tie Front Solid Tapered Pants" width="100" height="100">
+        <img src="/IS6465-GroupProject/image/ZipUpGraphicPrintFlareLegPants.jpg" alt="Zip Up Graphic Print Flare Leg Pants" width="100" height="100">
     </body>
 </html>
