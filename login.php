04564e910ba70302e82ca21b7a0220b84900c520
yagobaby <78980927+yagobaby@users.noreply.github.com>
2021-03-17 17:41:44 -0600

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
-$hn = 'localhost:3306'; //my datbase used port 3306, yours may be 3308 or something else
+$hn = 'localhost:3306'; //my database used port 3306, yours may be 3308 or something else
 $db = 'urbandb'; //I called my datbase urbandb, feel free to change to whatever you need
 $un = 'root';
 $pw = '';
