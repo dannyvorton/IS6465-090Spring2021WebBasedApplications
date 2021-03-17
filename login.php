687bebd390cc94619f21ec49cf63611bb6b97cad
yagobaby <78980927+yagobaby@users.noreply.github.com>
2021-03-17 17:41:09 -0600

    Add files via upload

---
+++ login.php
@@ -0,0 +1,7 @@
+<?php
+$hn = 'localhost:3306'; //my datbase used port 3306, yours may be 3308 or something else
+$db = 'urbandb'; //I called my datbase urbandb, feel free to change to whatever you need
+$un = 'root';
+$pw = '';
+
+?>
