603d42c6983d843f1c6d4eb971af7b7c75ede0de
yagobaby <78980927+yagobaby@users.noreply.github.com>
2021-03-19 17:42:23 -0600

    Add files via upload

--- DaveCode/check-session.php
+++ DaveCode/check-session.php
@@ -1,10 +1,10 @@
 <?php
 
-require_once 'User.php';
+require_once 'user.php';
 
 session_start();
 
-if(!isset($_SESSION['username'])){
+if(!isset($_SESSION['user'])){
 	header("Location: login-page1.php"); //login-page1.php is what I used for my own version of the setup for testing  purposes we just change it to the real login page
 	exit();
 }else{
--- DaveCode/login-page1.php
+++ DaveCode/login-page1.php
@@ -13,8 +13,10 @@
 
 <?php
 
+
 require_once 'login.php';
 require_once 'user.php';
+
 
 $conn = new mysqli($hn, $un, $pw, $db);
 if($conn->connect_error) die($conn->connect_error);
